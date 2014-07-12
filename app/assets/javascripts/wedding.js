fbbbd58e900a9d96b86c9edab449efa945f18992
Josh Newman <josh.newman4390@gmail.com>
2014-07-12 23:35:35 +1000

    Heaps of UI changes

---
+++ app/assets/javascripts/wedding.js
@@ -0,0 +1,6 @@
+// $("[href='#ceremony'], [href='#reception'], [href='#transport'], [href='#accomm']").each(function() {
+//   console.log("Selected hrefs")
+//   $(this).click(function() {
+//     $('body').scrollTo($(this).attr("href"));
+//   });
+// });
